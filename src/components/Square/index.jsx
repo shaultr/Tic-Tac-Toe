04e8aaed9d095224e83d48a2a440488366b045a9
unknown <a0548428751@gmail.com>
2024-05-07 16:52:49 +0300

    winner is big

--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -9,10 +9,17 @@ export default function Square({ winner, locations, index, click, board }) {
       <div className={styles.container} style={{ backgroundColor: thisColor }} onClick={() => click(index)}>
         {board[index] === 'x' && !winner && <img src='x.svg' width="70px" />}
         {board[index] === 'o' && !winner && <img src='o.svg' width="70px" />}
-        {board[index] === 'x' && winner === 'x' && <img src='x.svg' width="70px" />}
-        {board[index] === 'o' && winner==='x' && <img src='o-gray.svg' width="70px" />}
-        {board[index] === 'o' && winner === 'o' && <img src='o.svg' width="70px" />}
-        {board[index] === 'x' && winner==='o' && <img src='x-gray.svg' width="70px" />}
+
+        {/* if x is winner */}
+        {board[index] === 'x' && winner === 'x' && locations.includes(index) && <img src='x.svg' width="70px" />}
+        {board[index] === 'x' && winner === 'x' && !locations.includes(index) && <img src='x-gray.svg' width="70px" />}
+        {board[index] === 'o' && winner === 'x' && <img src='o-gray.svg' width="70px" />}
+
+        {/* if o is winner */}
+        {board[index] === 'o' && winner === 'o' && locations.includes(index) && <img src='o.svg' width="70px" />}
+        {board[index] === 'o' && winner === 'o' && !locations.includes(index) && <img src='o-gray.svg' width="70px" />}
+        {board[index] === 'x' && winner === 'o' && <img src='x-gray.svg' width="70px" />}
+
 
 
 
